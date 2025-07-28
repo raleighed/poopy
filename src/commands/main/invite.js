b5e839a9a849e7aaa50b3ea11c5ffdf85a682fce
raleigh <raleighed@proton.me>
2025-07-28 20:51:40 +0100

    So the bold actually Doesn't Worke.

--- src/commands/main/invite.js
+++ src/commands/main/invite.js
@@ -6,8 +6,8 @@ module.exports = {
     let bot = poopy.bot
 
     await msg.channel.sendTyping().catch(() => { })
-    var invite = `Wanna invite this bot? **[Click here.](<https://discord.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot%20applications.commands&permissions=275415166152>)**`
-      + (process.env.BOT_WEBSITE ? `\nWanna find out more about it? **[Check out the website.](${process.env.BOT_WEBSITE})**` : "")
+    var invite = `Wanna invite this bot? [Click here.](<https://discord.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot%20applications.commands&permissions=275415166152>)`
+      + (process.env.BOT_WEBSITE ? `\nWanna find out more about it? [Check out the website.](${process.env.BOT_WEBSITE})` : "")
 
     if (!msg.nosend) await msg.reply(invite).catch(() => { })
     return invite
