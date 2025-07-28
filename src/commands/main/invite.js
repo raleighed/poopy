184834817d4be1083e2de634b3a67a3d26f18370
raleigh <raleighed@proton.me>
2025-07-28 20:48:08 +0100

    made the website embed because why not

--- src/commands/main/invite.js
+++ src/commands/main/invite.js
@@ -7,7 +7,7 @@ module.exports = {
 
     await msg.channel.sendTyping().catch(() => { })
     var invite = `Wanna invite this bot? **[Click here.](<https://discord.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot%20applications.commands&permissions=275415166152>)**`
-      + (process.env.BOT_WEBSITE ? `\nWanna find out more about it? **[Check out the website.](<${process.env.BOT_WEBSITE}>)**` : "")
+      + (process.env.BOT_WEBSITE ? `\nWanna find out more about it? **[Check out the website.](${process.env.BOT_WEBSITE})**` : "")
 
     if (!msg.nosend) await msg.reply(invite).catch(() => { })
     return invite
